b917077f6c57f5502daa96da4cdb98224446fa34
aungphyo88 <aphyo@mail.sfsu.edu>
2020-02-20 00:20:54 -0800

    adding

---
+++ Intersection_Of_Two_LinkedList.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        Set<ListNode> set = new HashSet<ListNode>();
+
+        while(a != null){
+            set.add(a);
+            a = a.next;
+        }
+
+        while(b != null){
+            if(set.contains(b)){
+                return b;
+            }
+            b = b.next;
+        }
+
+        return null;
+    }
+}
+
+/**
+
+Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Reference of the node with value = 2
+Input Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [0,9,1,2,4]. From the head of B, it reads as [3,2,4].
+There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+
+**/
